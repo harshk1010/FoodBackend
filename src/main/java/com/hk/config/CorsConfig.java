0f6b1faa719aca52ea9ddac8d16a6e894edbd2a5
harshk1010 <113081552+harshk1010@users.noreply.github.com>
2025-04-17 05:30:11 +0530

    Update CorsConfig.java

--- src/main/java/com/hk/config/CorsConfig.java
+++ src/main/java/com/hk/config/CorsConfig.java
@@ -1,20 +1,3 @@
 package com.hk.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOriginPatterns("https://hk-food.netlify.app", "http://localhost:3000","https://hk-food-psi.vercel.app") // Allow only your frontends
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true)
-                .maxAge(3600); // Cache preflight response for 1 hour
-    }
-}
